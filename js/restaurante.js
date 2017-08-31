39b1142b5d20ccca4c3c939639b3ec61129d4cac
TheNewRol(Alfredo) <alfredo_2valle@hotmail.com>
2017-08-31 13:50:16 +0200

    Restaurante - V - 003

--- js/restaurante.js
+++ js/restaurante.js
@@ -1,3 +1,31 @@
 $( document ).ready(function(){
-    $(".button-collapse").sideNav();
+    function css(){
+        $(".button-collapse").sideNav();   
+    }
+    
+    function initMap(){
+        
+        var myLatLng = {lat: 41.9274705, lng: 2.2448564};
+        var map = new google.maps.Map(document.getElementById('map'), {
+            center: myLatLng,
+            //scrollwheel: false,
+            zoom: 17
+        });
+        //Creamos el marcador del mapa
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            animation: google.maps.Animation.DROP,
+            title: 'Nou Pamplona'
+        });
+        //añadiendo el marker al mapa
+        marker.setMap(map);
+    }
+    
+    function init(){
+        css();
+        initMap();
+    }
+    
+    init();
 });
