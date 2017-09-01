31c11bf920ee8cd3c96ecf416f417b7af9c6759e
oscar2909 <31469382+oscar2909@users.noreply.github.com>
2017-09-01 12:23:45 +0200

    V -004

--- js/lista.js
+++ js/lista.js
@@ -1,1 +1,29 @@
-$(".button-collapse").sideNav(); 
+$(document).ready(function(){
+      
+    function css(){
+        $(".button-collapse").sideNav();
+        
+        $('.modal').modal({
+                dismissible: true,
+                inDuration: 300, // Transition in duration
+                outDuration: 200
+            });
+        
+        
+        $(".modal-trigger").click(function(){
+             $('#modal1').modal('open');
+        });
+        $(".aceptar, cancelar").click(function(){
+            $('#modal1').modal('close');
+        });
+        
+        
+        
+    }
+      
+    function init(){
+        css();
+    }
+      
+    init();
+});
