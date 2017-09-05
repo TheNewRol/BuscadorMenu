15b185cb647b0e0bb532d52e5f1fe8a251734767
TheNewRol(Alfredo) <alfredo_2valle@hotmail.com>
2017-09-05 12:32:52 +0200

    Restaurante - V - 005

--- js/restaurante.js
+++ js/restaurante.js
@@ -2,6 +2,7 @@ $( document ).ready(function(){
     function css(){
         $('.modal').modal();
         $(".button-collapse").sideNav();
+         $('.carousel.carousel-slider').carousel({fullWidth: true});
     }
     
     function initMap(){
