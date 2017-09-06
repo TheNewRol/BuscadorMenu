a715a1d71ef5d436f01b808adeadbefdc12ba2c2
TheNewRol(Alfredo) <alfredo_2valle@hotmail.com>
2017-09-06 11:56:09 +0200

    Alta - V - 007

--- js/alta.js
+++ js/alta.js
@@ -1,14 +1,9 @@
 $(document).ready(function(){
+    var map, geocoder, marker;
+    var myLatLng = {lat: 41.9274705, lng: 2.2448564};
     
     function css(){
-        /*
-        Inicialización del menu hamburgesa
-        */
-        $(".button-collapse").sideNav();
         
-        /*
-        Colocar mapa dependiendo de si es movil o pc
-        */
         if($(window).width() + 17 < 767){
             $("#divmap div").attr("id", "");
                 $("#mapMovil div").attr("id", "map").css("height", "400px");
@@ -18,9 +13,7 @@ $(document).ready(function(){
             $("#divmap div").attr("id", "map");
             initMap();
         }
-        /*
-        Recolocar mapa en caso de modificar la anchura de la pantalla
-        */
+        
         $(window).resize(function() {
             console.log($(window).width()+17);
             if($(window).width() + 17 < 767){
@@ -38,25 +31,76 @@ $(document).ready(function(){
     
      function initMap(){
         
-        var myLatLng = {lat: 41.9274705, lng: 2.2448564};
-        var map = new google.maps.Map(document.getElementById('map'), {
+        geocoder = new google.maps.Geocoder();
+        
+        map = new google.maps.Map(document.getElementById('map'), {
             center: myLatLng,
             scrollwheel: false,
             zoom: 17
         });
+         
+         marker = new google.maps.Marker({});
         //Creamos el marcador del mapa
-        var marker = new google.maps.Marker({
+        /*marker = new google.maps.Marker({
+            draggable: true,
             position: myLatLng,
             map: map,
             animation: google.maps.Animation.DROP,
             title: 'Nou Pamplona'
         });
         //aÃ±adiendo el marker al mapa
-        marker.setMap(map);
+        marker.setMap(map);*/
+         
+        $("#provincia").bind("change paste keyup", obtenerDatos); 
+        $("#poblacion").bind("change paste keyup", obtenerDatos);
+    }
+    
+    function obtenerDatos(){
+        var provincia = $("#provincia").val();
+        var poblacion = $("#poblacion").val();
+        
+        if(provincia == undefined){
+            provincia = "";
+        }
+        if(poblacion == undefined){
+            poblacion = "";
+        }
+        
+        var address = provincia + ", " + poblacion;
+        
+        console.log(address);
+        
+        codeAddress(address);
+    }
+    
+    function codeAddress(address) {
+        geocoder.geocode({ 'address': address }, function(results, status) {
+          if (status === google.maps.GeocoderStatus.OK) {
+            map.setCenter(results[0].geometry.location);
+            marker.set
+            /*marker = new google.maps.Marker({
+                draggable: true,
+                map: map,
+                position: results[0].geometry.location
+            });*/
+            marker.addListener('click', toggleBounce);
+          } else {
+            //console.log("Geocode unsuccessful");
+          }
+        });
+    }
+    
+    function toggleBounce() {
+        if (marker.getAnimation() !== null) {
+          marker.setAnimation(null);
+        } else {
+          marker.setAnimation(google.maps.Animation.BOUNCE);
+        }
     }
     
     function init(){
         css();
+        //initMap();
     }
     
     init();
